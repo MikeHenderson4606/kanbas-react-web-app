769459c54f54bcc42d514d68ea822d3c6e9c4211
Michael Henderson <mikesmac@Michaels-MacBook-Pro.local>
2024-02-06 16:56:58 -0500

    Begun using reactJS

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Labs from './Labs';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const someFunction = (b: number) => b*b;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>
+        Labs
+      </h1>
+      <Labs />
+      <h1>
+        Kanbas
+      </h1>
+      <ul>
+        <li>
+          <a href='/Kanbas/Dashboard/screen.html'>
+            Kanbas
+          </a>
+        </li>
+      </ul>
+    </>
   );
 }
 
---
+++ src/Labs/index.tsx
@@ -0,0 +1,23 @@
+function Labs() {
+    const labData = [
+    {
+        name: 'Lab 1',
+        href: '/labs/a1'
+    },
+    {
+        name: 'Lab 2',
+        href: '/labs/a1'
+    },
+    {
+        name: 'Lab 3',
+        href: '/labs/a1'
+    }
+    ];
+
+    return (
+    <ul>
+    </ul>
+    );
+}
+
+export default Labs;
